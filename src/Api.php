d0a8f0b30faa14c9bdea0fa0113f9d87d3816f46
Dave Xie <Dave@corp.kucoin.com>
2022-02-27 10:05:44 +0800

    upgrade version

--- src/Api.php
+++ src/Api.php
@@ -16,12 +16,12 @@ abstract class Api
     /**
      * @var string SDK Version
      */
-    const VERSION = '1.0.18';
+    const VERSION = '1.0.19';
 
     /**
      * @var string SDK update date
      */
-    const UPDATE_DATE = '2021.12.02';
+    const UPDATE_DATE = '2022.02.27';
 
     /**
      * @var string
